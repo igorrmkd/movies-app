4617be551cc9954a1df732dff1b95b66bba19991
Igor Risteski <pirej30@yahoo.com>
2020-04-02 00:48:39 +0200

    first demo fetch test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: "c04285f4",
+            i: 'tt0848228'
+        }
+    });
+    console.log(response.data);
+
+};
+
+fetchData();
