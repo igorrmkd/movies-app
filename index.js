d3aea023728a8fc6b9bdb9278ecb0c53fd339a62
Igor Risteski <pirej30@yahoo.com>
2020-04-12 01:47:09 +0200

    Generate some html content, to show the search results on the page

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const fetchData = async (searchterm) => {
             s: searchterm
         }
     });
+
+    // if there is an response Error, i.e. the earched movie is not found.. return an empty array
+    if (response.data.Error) {
+        return [];
+    }
+
+    // return the data from Search
     return response.data.Search;
 
 };
@@ -14,7 +21,17 @@ const input = document.querySelector('input');
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
-    console.log(movies);
+
+    // generate some html content on the page, -> the search results
+    for (let movie of movies) {
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h1>${movie.Title}</h1>
+        `;
+        document.querySelector('#target').appendChild(div);
+    }
 
 };
 input.addEventListener('input', debounce(onInput, 500)); // 500ms delay
