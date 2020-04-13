4683a174283ca13505cf19fc77f37bd746b5d8db
Igor Risteski <pirej30@yahoo.com>
2020-04-13 03:13:36 +0200

    Reusable 4: Make the onMovieSelect and input value reusable

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ createAutoComplete({
             ${movie.Title}
             ${movie.Year}
         `;
+    },
+    onOptionSelect(movie) {
+        onMovieSelect(movie);
+    },
+    inputValue(movie) {
+        return movie.Title;
     }
 });
 
