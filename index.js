6f6795170f963dad0a2f689325cba47b3e4f0943
Igor Risteski <pirej30@yahoo.com>
2020-04-14 02:35:35 +0200

    Finally make the comparison, and add or remove some appropriate styling, to make the bigger value green, and smaller- yellow.

--- index.js
+++ index.js
@@ -75,8 +75,24 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 };
 
 const runComparison = () => {
-    console.log("runComparison");
+    const leftSideStats = document.querySelectorAll('#left-summary .notification');
+    const rightSideStats = document.querySelectorAll('#right-summary .notification');
 
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index];
+
+        const leftSideValue = parseInt(leftStat.dataset.value);
+        const rightSideValue = parseInt(rightStat.dataset.value);
+
+        if (rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary');
+            leftStat.classList.add('is-warning');
+        } else {
+            rightStat.classList.remove('is-primary');
+            rightStat.classList.add('is-warning');
+        }
+
+    });
 };
 
 // a template for showing selected movie data, fetched from the API
